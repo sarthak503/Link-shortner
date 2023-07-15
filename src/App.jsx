4145443b679998e5900fc3ce003bc80452de8acf
sarthak503 <sarthak.kumar503@gmail.com>
2023-07-16 00:17:12 +0530

    added functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import Shortner from "./Shortner"
+import { useState } from "react"
 import './App.css'
+import Shortner from "./Shortner"
 import BackgroundAnimate from "./BackgroundAnimate"
+import  LinkResult  from "./LinkResult"
 function App() {
-
+  const [inputValue,setInputValue]=useState("");
   return (
     <div className="container">
-      <Shortner/>
+      <Shortner setInputValue={setInputValue}/>
       <BackgroundAnimate/>
+      <LinkResult inputValue={inputValue}/>
     </div>
   )
 }
--- src/Shortner.jsx
+++ src/Shortner.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const Shortner = () => {
+const Shortner = ({setInputValue}) => {
+  const [value,setValue]=useState("");
+  const handleClick =()=>{
+    setInputValue(value);
+    setValue("")
+  }
   return (
    <div className="input-container">
     <h1>URL <span>Shortener</span></h1>
     <div>
-      <input type="text" placeholder='Paste the link'/>
-      <button>Shorten</button>
+      <input 
+      type="text" 
+      placeholder='Paste the link'
+      value={value}
+      onChange={e=>setValue(e.target.value)}
+      />
+      <button onClick={handleClick}>Shorten</button>
     </div>
    </div>
   )
